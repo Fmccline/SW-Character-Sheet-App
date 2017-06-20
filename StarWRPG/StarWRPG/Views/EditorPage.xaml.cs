60244d1db71436e45ea9fb4f7bbccf138a0975fb
Frank Cline <frankmcline@gmail.com>
2017-06-20 14:47:53 -0700

    Changed EditorPage from a c# page to a xaml/c# page

---
+++ StarWRPG/StarWRPG/Views/EditorPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using StarWRPG.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StarWRPG.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorPage : ContentPage
+    {
+        string binding;
+
+        public EditorPage(string title, string binding, FaDCharacterViewModel characterViewModel)
+        {
+            InitializeComponent();
+            BindingContext = characterViewModel;
+            this.binding = binding;
+
+            TitleLabel.Text = title;
+            Editor.SetBinding(Editor.TextProperty, binding, BindingMode.OneWay);
+        }
+
+        private async void CancelClickedAsync(object sender, EventArgs e)
+        {
+            var answer = await DisplayAlert("Are you sure?", "Your changes will not be saved.", "Yes", "No");
+            if (answer)
+            {
+                await Navigation.PopModalAsync();
+            }
+        }
+
+        private async void AcceptClickedAsync(object sender, EventArgs e)
+        {
+            Editor.SetBinding(Editor.TextProperty, binding, BindingMode.OneWayToSource);
+            await Navigation.PopModalAsync();
+        }
+    }
+}
