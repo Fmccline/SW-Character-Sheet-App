01a6ede8e758645f4d059ecb858bc1570c31f5ae
Frank Cline <frankmcline@gmail.com>
2017-08-18 17:13:48 -0700

    Added app icon to android. Also, created a custom editor renderer in android that removes the underline, and it has a default placeholder (kind of)

---
+++ StarWRPG/Droid/CustomRenderers/CustomEditorRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using StarWRPG.Views;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.Android;
+using StarWRPG.Droid;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CustomEditorRenderer))]
+namespace StarWRPG.Droid
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            Control?.SetBackgroundColor(Android.Graphics.Color.Transparent);
+
+            if (Control != null && String.IsNullOrWhiteSpace(Control.Text))
+            {
+                string placeholder = "Input text here...";
+                Control.SetText(placeholder.ToCharArray(), 0, placeholder.Length);
+            }
+        }
+    }
+}
---
+++ StarWRPG/iOS/CustomRenderers/CustomNavigationRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using StarWRPG.Views;
+using StarWRPG.iOS;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CustomNavigationRenderer))]
+namespace StarWRPG.iOS
+{
+    public class CustomNavigationRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                var page = e.NewElement as Page;
+                NavigationPage.SetBackButtonTitle(page, "");
+            }
+        }
+    }
+}
