112f2b5a900cf02d406ef22a1f4b474740992529
Frank Cline <frankmcline@gmail.com>
2017-08-22 14:42:53 -0800

    Fixed an issue with iOS that made the SearchBars always white

---
+++ StarWRPG/iOS/CustomRenderers/CustomSearchBarRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using StarWRPG.Views;
+using StarWRPG.iOS;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+[assembly: ExportRenderer(typeof(CustomSearchBar), typeof(CustomSearchBarRenderer))]
+namespace StarWRPG.iOS
+{
+    public class CustomSearchBarRenderer : SearchBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SearchBarStyle = UISearchBarStyle.Minimal;
+            }
+        }
+    }
+}
